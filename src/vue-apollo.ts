adf669ae340258e75093a75140423751ee16a10d
b.johnson <b.johnson@fan-gene.com>
2021-08-15 19:11:27 -0400

    remove comment

--- src/vue-apollo.ts
+++ src/vue-apollo.ts
@@ -17,7 +17,6 @@ const defaultOptions = {
   httpEndpoint,
   // You can use `wss` for secure connection (recommended in production)
   // Use `null` to disable subscriptions
-  // wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'ws://localhost:3000/graphql',
   wsEndpoint: process.env.VUE_APP_GRAPHQL_WS || 'ws://localhost:3000/graphql',
   // LocalStorage token
   tokenName: AUTH_TOKEN,
